c618b716dd57b68c373fdbf780024b8011712ed2
sailesh <vermasailesh271@gmail.com>
2023-11-14 19:18:10 +0530

    added image and text state provider

---
+++ lib/providers/image_provider_state.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PositionedImageState {
+  final String id;
+  final String image;
+  final Offset offset;
+  final double scaleMultiplier;
+
+  const PositionedImageState({
+    required this.id,
+    required this.image,
+    required this.offset,
+    required this.scaleMultiplier,
+  });
+
+  PositionedImageState copyWith({
+    String? id,
+    String? image,
+    Offset? offset,
+    double? scaleMultiplier,
+  }) {
+    return PositionedImageState(
+      id: id ?? this.id,
+      image: image ?? this.image,
+      offset: offset ?? this.offset,
+      scaleMultiplier: scaleMultiplier ?? this.scaleMultiplier,
+    );
+  }
+}
+
+class ImagesStateNotifier extends StateNotifier<List<PositionedImageState>> {
+  ImagesStateNotifier(this.ref) : super([]);
+
+  Ref ref;
+
+  void addImageToStateList({
+    required String image,
+  }) {
+    final newList = state;
+
+    final newOffset = state.isEmpty
+        ? const Offset(80, 100)
+        : Offset(
+            state[state.length - 1].offset.dx - 20,
+            state[state.length - 1].offset.dy - 20,
+          );
+
+    newList.add(PositionedImageState(
+      id: DateTime.now().toIso8601String(),
+      offset: newOffset,
+      scaleMultiplier: 1,
+      image: image,
+    ));
+
+    state = [...newList];
+  }
+
+  void updateMultiplier({
+    required String id,
+    required double multiplier,
+  }) {
+    final newList = state;
+    var item = newList.firstWhere((element) => element.id == id);
+
+    final updatedItem = item.copyWith(
+      scaleMultiplier: multiplier,
+    );
+
+    newList[newList.indexWhere((element) => element.id == id)] = updatedItem;
+
+    state = [...newList];
+  }
+
+  void updateTextOffset({
+    required String id,
+    required Offset offset,
+  }) {
+    final newList = [...state];
+    var item = newList.firstWhere((element) => element.id == id);
+
+    final updatedItem = item.copyWith(
+      offset: offset,
+    );
+
+    newList[newList.indexWhere((element) => element.id == id)] = updatedItem;
+
+    state = [...newList];
+  }
+
+  void resetState() {
+    ref.invalidate(imagesStateProvider);
+  }
+}
+
+final imagesStateProvider =
+    StateNotifierProvider<ImagesStateNotifier, List<PositionedImageState>>(
+  (ref) => ImagesStateNotifier(ref),
+);
---
+++ lib/providers/text_provider_state.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PositionedTextState {
+  final String id;
+  final String text;
+  final Offset offset;
+  final double scaleMultiplier;
+
+  const PositionedTextState({
+    required this.id,
+    required this.text,
+    required this.offset,
+    required this.scaleMultiplier,
+  });
+
+  PositionedTextState copyWith({
+    String? id,
+    String? text,
+    Offset? offset,
+    double? scaleMultiplier,
+  }) {
+    return PositionedTextState(
+      id: id ?? this.id,
+      text: text ?? this.text,
+      offset: offset ?? this.offset,
+      scaleMultiplier: scaleMultiplier ?? this.scaleMultiplier,
+    );
+  }
+}
+
+class TextStateNotifier extends StateNotifier<List<PositionedTextState>> {
+  TextStateNotifier(this.ref) : super([]);
+  Ref ref;
+
+  void addTextToStateList({
+    required String text,
+    // required Offset offset,
+    required double multiplier,
+  }) {
+    final newList = state;
+
+    final newOffset = state.isEmpty
+        ? const Offset(80, 100)
+        : Offset(
+            state[state.length - 1].offset.dx - 20,
+            state[state.length - 1].offset.dy - 20,
+          );
+    newList.add(PositionedTextState(
+      id: DateTime.now().toString() + '@42532532352r',
+      offset: newOffset,
+      scaleMultiplier: multiplier,
+      text: text,
+    ));
+
+    state = [...newList];
+  }
+
+  void updateMultiplier({
+    required String id,
+    required double multiplier,
+  }) {
+    final newList = state;
+    var item = newList.firstWhere((element) => element.id == id);
+
+    final updatedItem = item.copyWith(
+      scaleMultiplier: multiplier,
+    );
+
+    newList[newList.indexWhere((element) => element.id == id)] = updatedItem;
+
+    state = [...newList];
+  }
+
+  void updateTextOffset({
+    required String id,
+    required Offset offset,
+  }) {
+    final newList = state;
+    var item = newList.firstWhere((element) => element.id == id);
+
+    final updatedItem = item.copyWith(
+      offset: offset,
+    );
+
+    newList[newList.indexWhere((element) => element.id == id)] = updatedItem;
+
+    state = [...newList];
+  }
+
+  void resetState() {
+    ref.invalidate(textStateProvider);
+  }
+}
+
+final textStateProvider =
+    StateNotifierProvider<TextStateNotifier, List<PositionedTextState>>(
+  (ref) => TextStateNotifier(ref),
+);
